bcaec7434a0401760475ec335339d9f613d4c7e4
Nurtau <nurtaut@gmail.com>
2021-03-15 13:09:23 +0600

    changing time implemented

---
+++ app.js
@@ -0,0 +1,30 @@
+"use strict"
+
+const clock = document.querySelector("#clock");
+
+const updateTime = () => {
+    const currentTime = new Date();
+    const options = {
+        second: "numeric",
+        minute: "numeric",
+        hours: "numeric",
+        day: "numeric",
+        month: "numeric",
+        year: "numeric",
+        weekday: "short"
+    };
+    const formatTime = new Intl.DateTimeFormat(navigator.language, options).format(currentTime);
+    clock.textContent = formatTime;
+}
+
+window.addEventListener("load", event => {
+    event.preventDefault();
+    updateTime();
+    const milliseconds = 1000 - new Date().getMilliseconds();
+    setTimeout(() => {
+        setInterval(() => {
+            updateTime();
+        }, 1000);
+        updateTime();
+    }, milliseconds);
+});
